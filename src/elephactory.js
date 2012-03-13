3f166bb9989ea2707dadffce240dcc1e5edfe96c
Rich Davis <richjddavis@gmail.com>
2012-03-13 22:41:22 +1000

    Preliminary work on breaking out core functionality, allowing for target environment specifics (and other extensions) to extend by either monkeypatching or adding to the prototype chain

--- src/elephactory.js
+++ src/elephactory.js
@@ -2,79 +2,85 @@
 // List of elements taken from http://joshduck.com/periodic-table.html
 var _aElements = ['html', 'col', 'table', 'head', 'span', 'div', 'fieldset', 'form', 'body', 'h1', 'section', 'colgroup', 'tr', 'title', 'a', 'pre', 'meter', 'select', 'aside', 'h2', 'header', 'caption', 'td', 'meta', 'rt', 'dfn', 'em', 'i', 'small', 'ins', 's', 'br', 'p', 'blockquote', 'legend', 'optgroup', 'address', 'h3', 'nav', 'menu', 'th', 'base', 'rp', 'abbr', 'time', 'b', 'strong', 'del', 'kbd', 'hr', 'ol', 'dl', 'label', 'option', 'datalist', 'h4', 'article', 'command', 'tbody', 'link', 'noscript', 'q', 'var', 'sub', 'mark', 'bdi', 'wbr', 'figcaption', 'ul', 'dt', 'input', 'output', 'keygen', 'h5', 'footer', 'summary', 'thead', 'style', 'script', 'cite', 'samp', 'sup', 'ruby', 'bdo', 'code', 'figure', 'li', 'dd', 'textarea', 'button', 'progress', 'h6', 'hgroup', 'details', 'tfoot', 'img', 'area', 'map', 'embed', 'object', 'param', 'source', 'iframe', 'canvas', 'track', 'audio', 'video'];
 
-var _isAttributesObject = function (oObject) {
-	return (oObject !== null && typeof oObject === 'object' && !_isDOMNode(oObject));
+// CONSTRUCTOR
+var Elephactory = function (oDocument) {
+	this.oDocument = oDocument;
 };
-var _isDOMNode = function (mNode) {
+
+// UTILITIES (that we might want to monkeypatch)
+Elephactory.prototype._isAttributesObject = function (oObject) {
+	return (oObject !== null && typeof oObject === 'object' && !this._isDOMNode(oObject));
+};
+Elephactory.prototype._isDOMNode = function (mNode) {
 	return ('nodeName' in mNode && 'nodeType' in mNode);
 };
-var _addEventListener = function (sEvent, fnCallback) {
-	// TODO: Should we normalise the event handlers, or leave it up to the dev to do this?
-	if ('addEventListener' in this) {
-		return this.addEventListener(sEvent, fnCallback, false);
-	} else if ('attachEvent' in this) {
-		return this.attachEvent(sEvent, fnCallback);
-	} else {
-		throw new Error('No recognised event binding method found');
-	}
+Elephactory.prototype._addEventListener = function (oElement, sEvent, fnCallback) {
+	return oElement.addEventListener(sEvent, fnCallback);
 };
-var _createElement = function (sTag, oAttributes) {
-	// TODO: Old IEs have issues with input elements and certain attributes being set after creation
+Elephactory.prototype._createElement = function (sTag, oAttributes) {
 	var oElement;
 
 	oElement = this.createElement(sTag);
-	_setAttributes.call(oElement, oAttributes);
+	this._setAttributes(oElement, oAttributes);
 
 	return oElement;
 };
-var _setAttributes = function (oAttributes) {
-	var sAttribute,
-		mValue,
-		aMatches,
+Elephactory.prototype._setAttributes = function (oElement, oAttributes) {
+	var sAttribute;
+	for (sAttribute in oAttributes) {
+		this._setAttribute(oElement, sAttribute, oAttributes[sAttribute]);
+	}
+};
+Elephactory.prototype._setAttribute = function (oElement, sAttribute, mValue) {
+	var	aMatches,
 		aHandlers,
 		i, l;
-	// TODO: Old IEs have issues with elements and certain attributes being set after creation
-	// http://webbugtrack.blogspot.com.au/2007/08/bug-242-setattribute-doesnt-always-work.html
-	for (sAttribute in oAttributes) {
-		mValue = oAttributes[sAttribute];
-		if (oAttributes.hasOwnProperty(sAttribute)) {
-			aMatches = sAttribute.match(/^on([a-z]+)$/);
-			if (aMatches) {
-				// Event Handlers (Array or single function)
-				aHandlers = Array.isArray(mValue) ? mValue : [mValue];
-				for (i = 0, l = mValue.length; i < l; i++) {
-					if (typeof mValue[i] === 'function') {
-						_addEventListener.call(this, aMatches[0], mValue);
-					}
+	if (oAttributes.hasOwnProperty(sAttribute)) {
+		aMatches = sAttribute.match(/^on([a-z]+)$/);
+		if (aMatches) {
+			// Event Handlers (Array or single function)
+			aHandlers = Array.isArray(mValue) ? mValue : [mValue];
+			for (i = 0, l = mValue.length; i < l; i++) {
+				if (typeof mValue[i] === 'function') {
+					this._addEventListener(aMatches[0], mValue);
 				}
-			} else if (mValue === true) {
-				// Boolean TRUE: emulate XHTML behaviour
-				this.setAttribute(sAttribute, sAttribute);
-			} else if (mValue === false || mValue === null) {
-				// Boolean FALSE or NULL: treat as removal
-				this.removeAttribute(sAttribute);
-			} else {
-				// Anything else
-				// FIXME: http://webbugtrack.blogspot.com.au/2007/08/bug-242-setattribute-doesnt-always-work.html
-				this.setAttribute(sAttribute, mValue);
 			}
+		} else if (mValue === true) {
+			// Boolean TRUE: emulate XHTML behaviour
+			oElement.setAttribute(sAttribute, sAttribute);
+		} else if (mValue === false || mValue === null) {
+			// Boolean FALSE or NULL: treat as removal
+			oElement.removeAttribute(sAttribute);
+		} else {
+			// Anything else
+			oElement.setAttribute(sAttribute, mValue);
 		}
 	}
 };
-var _toDOMNode = function (mNode) {
-	// TODO: Add in a way to add custom handlers
-	if (_isDOMNode(mNode)) { // DOM Node
+Elephactory.prototype._toDOMNode = function (mNode) {
+	// TODO: Add in a way to add custom handlers, or will monkeypatching provide this for us?
+	if (this._isDOMNode(mNode)) { // DOM Node
 		return mNode;
 	} else if (typeof mNode === 'string' || typeof mNode === 'number') { // Anything else that's useful to display
-		return this.createTextNode(mNode);
+		return this.oDocument.createTextNode(mNode);
 	}
 	return null;
 };
+Elephactory.prototype._appendChildren = function (oNode, aChildren) {
+	var i, l, oChildNode;
+	for (i = 0, l = aChildren.length; i < l; i++) {
+		oChildNode = this._toDOMNode(aChildren[i]);
+		if (oChildNode) {
+			oNode.appendChild(oChildNode);
+		}
+	}
+};
 
+// FACTORIES
 // Element Factory
-var _elementFactory = function (sTag, oAttributes) {
+Elephactory.prototype._element = function () {
 	var oElement, aChildren;
-	if (_isAttributesObject(oAttributes)) {
+	if (this._isAttributesObject(oAttributes)) {
 		aChildren = Array.prototype.slice.call(arguments, 2);
 	} else {
 		oAttributes = {};
@@ -82,36 +88,24 @@ var _elementFactory = function (sTag, oAttributes) {
 	}
 
 	// Create our Element
-	oElement = _createElement.call(this, sTag, oAttributes);
+	oElement = this._createElement(this, sTag, oAttributes);
 
 	// Set attributes
-	_setAttributes.call(oElement, oAttributes);
+	this._setAttributes(oElement, oAttributes);
 
 	// Append children
-	var i, l, oChildNode;
-	for (i = 0, l = aChildren.length; i < l; i++) {
-		oChildNode = _toDOMNode.call(this, aChildren[i]);
-		if (oChildNode) {
-			oElement.appendChild(oChildNode);
-		}
-	}
+	this._appendChildren(oElement, aChildren);
 
 	return oElement;
 };
 
-// DOM Fragment Factory
-var _fragmentFactory = function () {
+// Fragment Factory
+Elephactory.prototype._fragment = function () {
 	var aChildren = Array.prototype.slice.call(arguments, 0),
-		oFragment = this.createDocumentFragment();
+		oFragment = this.oDocument.createDocumentFragment();
 
 	// Append children
-	var i, l, oChildNode;
-	for (i = 0, l = aChildren.length; i < l; i++) {
-		oChildNode = _toDOMNode.call(this, aChildren[i]);
-		if (oChildNode) {
-			oElement.appendChild(oChildNode);
-		}
-	}
+	this._appendChildren(oFragment, aChildren);
 
 	return oFragment;
 };
@@ -119,10 +113,7 @@ var _fragmentFactory = function () {
 // EXPORTS
 exports.for = function (oDocument) {
 	// TODO: Do we want to memoise?
-	var oInstance = {
-		fragment : _fragmentFactory.bind(oDocument),
-		factory : _elementFactory.bind(oDocument)
-	};
+	var oInstance = new Elephactory(oDocument);
 
 	var i, l;
 	for (i = 0, l = _aElements.length; i < l; i++) {
@@ -131,3 +122,4 @@ exports.for = function (oDocument) {
 
 	return oInstance;
 };
+exports.Elephactory = Elephactory;
---
+++ src/elephactory/legacybrowsers.js
@@ -0,0 +1,3 @@
+// Monkeypatch Elephactory to fix issues with terrible browsers
+// TODO
+// FIXME: http://webbugtrack.blogspot.com.au/2007/08/bug-242-setattribute-doesnt-always-work.html
